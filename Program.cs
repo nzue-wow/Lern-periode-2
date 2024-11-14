dec97e62ca06d39b8d7a48a6ff159ce1c5cd5494
nzue-wow <N.zuehlke.inf24@stud.bbbaden.ch>
2024-11-14 22:15:39 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,12 +10,20 @@ namespace Snakespiel
         static List<(int x, int y)> snake = new List<(int x, int y)>();
         static int richtungX = 1;
         static int richtungY = 0;
+        static bool spielLäuft = true;
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
             InitialisiereFeld();
             startPosition();
 
-            feldzeichen();
+            while (spielLäuft)
+            {
+                feldzeichen();
+                schlangeBewegen();
+                TastenKontrolle();
+            }
+           
 
         }
 
@@ -50,7 +58,8 @@ namespace Snakespiel
 
         static void feldzeichen()
         {
-            for(int y = 0;y < höhe; y++)
+            Console.SetCursorPosition(0, 0);
+            for (int y = 0;y < höhe; y++)
             {
                 for(int x = 0;x < breite; x++)
                 {
@@ -64,6 +73,59 @@ namespace Snakespiel
         {
             int neuX = snake[0].x + richtungX;
             int neuY = snake[0].y + richtungY;
+
+            if (neuX <= 0 || neuX >= breite - 1 || neuY <= 0 || neuY >= höhe - 1)
+            {
+                spielLäuft = false;
+                Console.Clear();
+                Console.WriteLine("Game Over");
+                return;
+            }
+
+            // Neuer Kopf
+            Feld[snake[0].x, snake[0].y] = '-';
+            snake.Insert(0, (neuX, neuY));
+            Feld[neuX, neuY] = KopfSymbol();
+
+            // Schwanz entfernen für eine gleiche länge der Schlange
+            var schwanz = snake[snake.Count - 1];
+            Feld[schwanz.x, schwanz.y] = ' ';
+            snake.RemoveAt(snake.Count - 1);
+        }
+
+        static void TastenKontrolle()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(intercept: true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (richtungY == 0) { richtungX = 0; richtungY = -1; }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (richtungY == 0) { richtungX = 0; richtungY = 1; }
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (richtungY == 0) { richtungX = -1; richtungY = 0; }
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        if (richtungY == 0) { richtungX = 1; richtungY = 0; }
+                        break;
+                }
+            }
+        }
+
+        static char KopfSymbol()
+        {
+            if (richtungX == 1) return '>'; // rechts
+            if (richtungX == -1) return '<'; // links
+            if (richtungY == -1) return '^'; // oben
+            if (richtungY == 1) return 'v'; //unten
+            return '>';
         }
     }
 }
